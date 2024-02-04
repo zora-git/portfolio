d9562006885fdaefa2cdd566982c17b07dbae482
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-04 19:27:40 +0900

    added ball cursor animation on index

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// create instance of kinet with custom settings
+var kinet = new Kinet({
+  acceleration: 0.03,
+  friction: 0.4,
+  names: ["x", "y"],
+});
+
+// select circle element
+var circle = document.getElementById("circle");
+
+// set handler on kinet tick event
+kinet.on("tick", function (instances) {
+  circle.style.transform = `translate3d(${instances.x.current}px, ${
+    instances.y.current
+  }px, 0) rotateX(${instances.x.velocity / 2}deg) rotateY(${
+    instances.y.velocity / 2
+  }deg)`;
+});
+
+// call kinet animate method on mousemove
+document.addEventListener("mousemove", function (event) {
+  kinet.animate("x", event.clientX - window.innerWidth / 2);
+  kinet.animate("y", event.clientY - window.innerHeight / 2);
+});
